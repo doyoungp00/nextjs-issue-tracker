1fcbe336ea4e3d43b1cf13f608d7562c5e8239d2
Doyoung Park <doyoungp.00@gmail.com>
2024-08-16 16:33:47 +0900

    Client side form validation

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,18 +1,9 @@
+import { createIssueSchema } from "@/app/validateSchema";
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-import { z } from "zod";
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
-
-  // Define validation schema
-  const createIssueSchema = z.object({
-    title: z
-      .string()
-      .min(1, "Title is required.")
-      .max(255, "Title is too long."),
-    description: z.string().min(1, "Description is required."),
-  });
 
   // Validate data
   const validation = createIssueSchema.safeParse(body);
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { Button, Callout, TextField } from "@radix-ui/themes";
+import { createIssueSchema } from "@/app/validateSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
@@ -8,47 +10,64 @@ import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { AiFillExclamationCircle } from "react-icons/ai";
 import SimpleMDE from "react-simplemde-editor";
+import { z } from "zod";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 function NewIssuePage() {
-  const { handleSubmit, register, control } = useForm<IssueForm>();
+  const {
+    handleSubmit,
+    register,
+    control,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   const router = useRouter();
   const [error, setError] = useState("");
 
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        try {
-          await axios.post("/api/issues", data);
-          router.push("/issues");
-        } catch (error) {
-          setError("An unexpected error occurred.");
-        }
-      })}
-    >
-      <TextField.Root placeholder="Title" {...register("title")} />
-      <Controller
-        name="description"
-        control={control}
-        render={({ field: props }) => (
-          <SimpleMDE placeholder="Description" {...props} />
+    <div className="max-w-xl">
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("An unexpected error occurred.");
+          }
+        })}
+      >
+        <TextField.Root placeholder="Title" {...register("title")} />
+        {errors.title && (
+          <Text color="red" as="p">
+            {errors.title.message}
+          </Text>
         )}
-      />
-      {error && (
-        <Callout.Root color="red" role="alert">
-          <Callout.Icon>
-            <AiFillExclamationCircle />
-          </Callout.Icon>
-          <Callout.Text>{error}</Callout.Text>
-        </Callout.Root>
-      )}
-      <Button>Submit new Issue</Button>
-    </form>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field: props }) => (
+            <SimpleMDE placeholder="Description" {...props} />
+          )}
+        />
+        {errors.description && (
+          <Text color="red" as="p">
+            {errors.description.message}
+          </Text>
+        )}
+        {error && (
+          <Callout.Root color="red" role="alert">
+            <Callout.Icon>
+              <AiFillExclamationCircle />
+            </Callout.Icon>
+            <Callout.Text>{error}</Callout.Text>
+          </Callout.Root>
+        )}
+        <Button>Submit new Issue</Button>
+      </form>
+    </div>
   );
 }
 
