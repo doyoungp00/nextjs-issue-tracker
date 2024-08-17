c6c0770023455fe03fdd150db5e4fe52bb27f555
Doyoung Park <doyoungp.00@gmail.com>
2024-08-17 14:51:26 +0900

    Show issues

---
+++ app/components/IssueStatus.tsx
@@ -0,0 +1,32 @@
+import {
+  AiFillCheckCircle,
+  AiFillPlusCircle,
+  AiFillRightCircle,
+} from "react-icons/ai";
+
+function IssueStatus({ status }: { status: string }) {
+  return (
+    <div className="flex items-center gap-2">
+      {status === "OPEN" && (
+        <>
+          <AiFillPlusCircle color="orange" />
+          <span className="text-orange-400">OPEN</span>
+        </>
+      )}
+      {status === "IN_PROGRESS" && (
+        <>
+          <AiFillRightCircle color="blue" />
+          <span className="text-blue-600">IN PROGRESS</span>
+        </>
+      )}
+      {status === "CLOSED" && (
+        <>
+          <AiFillCheckCircle color="green" />
+          <span className="text-green-600">CLOSED</span>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default IssueStatus;
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,5 +1,52 @@
-function IssuesPage() {
-  return <div>Issues Page</div>;
+import prisma from "@/prisma/client";
+import { Button, Table } from "@radix-ui/themes";
+import Link from "next/link";
+import IssueStatus from "../components/IssueStatus";
+
+async function IssuesPage() {
+  const issues = await prisma.issue.findMany();
+
+  return (
+    <div>
+      <div className="mb-5">
+        <Button>
+          <Link href="/issues/new">New Issue</Link>
+        </Button>
+      </div>
+
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                {issue.title}
+                <div className="block md:hidden">
+                  <IssueStatus status={issue.status} />
+                </div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                <IssueStatus status={issue.status} />
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.createdAt.toLocaleString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </div>
+  );
 }
 
 export default IssuesPage;
