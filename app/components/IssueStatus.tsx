2877b3e829bd15bba4619cda81ac290265029e81
Doyoung Park <doyoungp.00@gmail.com>
2024-08-18 16:07:44 +0900

    Update IssueStatus

--- app/components/IssueStatus.tsx
+++ app/components/IssueStatus.tsx
@@ -1,31 +1,53 @@
+import { Status } from "@prisma/client";
+import { Badge } from "@radix-ui/themes";
+import { IconType } from "react-icons";
 import {
   AiFillCheckCircle,
   AiFillPlusCircle,
   AiFillRightCircle,
 } from "react-icons/ai";
 
-function IssueStatus({ status }: { status: string }) {
+const statusStyles: Record<
+  Status,
+  {
+    label: string;
+    badgeColor: "orange" | "blue" | "green";
+    icon: IconType;
+    textColor: string;
+  }
+> = {
+  OPEN: {
+    label: "Open",
+    badgeColor: "orange",
+    icon: AiFillPlusCircle,
+    textColor: "text-orange-400",
+  },
+  IN_PROGRESS: {
+    label: "In Progress",
+    badgeColor: "blue",
+    icon: AiFillRightCircle,
+    textColor: "text-blue-600",
+  },
+  CLOSED: {
+    label: "Closed",
+    badgeColor: "green",
+    icon: AiFillCheckCircle,
+    textColor: "text-green-600",
+  },
+};
+
+function IssueStatus({ status }: { status: Status }) {
+  const {
+    label,
+    badgeColor,
+    icon: StatusIcon,
+    textColor,
+  } = statusStyles[status];
   return (
-    <div className="flex items-center gap-1">
-      {status === "OPEN" && (
-        <>
-          <AiFillPlusCircle color="orange" />
-          <span className="text-orange-400">OPEN</span>
-        </>
-      )}
-      {status === "IN_PROGRESS" && (
-        <>
-          <AiFillRightCircle color="blue" />
-          <span className="text-blue-600">IN PROGRESS</span>
-        </>
-      )}
-      {status === "CLOSED" && (
-        <>
-          <AiFillCheckCircle color="green" />
-          <span className="text-green-600">CLOSED</span>
-        </>
-      )}
-    </div>
+    <Badge className="flex items-center gap-1" color={badgeColor}>
+      <StatusIcon color={badgeColor} />
+      <span className={textColor}>{label}</span>
+    </Badge>
   );
 }
 
