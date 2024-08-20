e77d51e623f700f0a69a2978a24ce34f2797b1a0
Doyoung Park <doyoungp.00@gmail.com>
2024-08-20 22:55:00 +0900

    Update new issue button

--- app/issues/IssueActions.tsx
+++ app/issues/IssueActions.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@radix-ui/themes";
 import Link from "next/link";
+import { AiOutlinePlus } from "react-icons/ai";
 
 function IssueActions() {
   return (
     <div className="mb-5">
       <Button>
+        <AiOutlinePlus />
         <Link href="/issues/new">New Issue</Link>
       </Button>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
-import { Button } from "@radix-ui/themes";
-import Link from "next/link";
+import IssueActions from "./issues/IssueActions";
 
 export default function Home() {
-  return (
-    <div>
-      <Button>
-        <Link href="/issues/new">New Issue</Link>
-      </Button>
-    </div>
-  );
+  return <IssueActions />;
 }
