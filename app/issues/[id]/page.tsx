328b1cff319aa6f26731b2501823f27ea55cbf12
Doyoung Park <doyoungp.00@gmail.com>
2024-08-20 16:00:40 +0900

    Add edit issue button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,8 +1,10 @@
 import { IssueStatus } from "@/app/components";
 import prisma from "@/prisma/client";
-import { Box, Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
 import delay from "delay";
+import Link from "next/link";
 import { notFound } from "next/navigation";
+import { AiOutlineEdit } from "react-icons/ai";
 import ReactMarkdown from "react-markdown";
 
 interface Props {
@@ -16,16 +18,24 @@ async function IssueDetailsPage({ params: { id } }: Props) {
   await delay(1000);
 
   return (
-    <Box className="max-w-xl">
-      <Heading>{issue?.title}</Heading>
-      <Flex gap="2" my="2">
-        <IssueStatus status={issue.status} />
-        <Text>{issue?.createdAt.toLocaleString()}</Text>
-      </Flex>
-      <Card className="prose" mt="4">
-        <ReactMarkdown>{issue?.description}</ReactMarkdown>
-      </Card>
-    </Box>
+    <Grid columns={{ initial: "1", md: "2" }} gap="4">
+      <Box>
+        <Heading>{issue?.title}</Heading>
+        <Flex gap="2" my="2">
+          <IssueStatus status={issue.status} />
+          <Text>{issue?.createdAt.toLocaleString()}</Text>
+        </Flex>
+        <Card className="prose" mt="4">
+          <ReactMarkdown>{issue?.description}</ReactMarkdown>
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <AiOutlineEdit />
+          <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+        </Button>
+      </Box>
+    </Grid>
   );
 }
 
