a03b3d173f5553d228d7d45dd58e1699440ae1ab
Doyoung Park <doyoungp.00@gmail.com>
2024-08-21 17:34:34 +0900

    Populate assignee selector

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,15 +1,31 @@
 "use client";
 
+import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function AssigneeSelect() {
+  const [users, setUsers] = useState<User[]>([]);
+  useEffect(() => {
+    async function fetchUsers() {
+      const { data } = await axios.get<User[]>("/api/users");
+      setUsers(data);
+    }
+    fetchUsers();
+  }, []);
+
   return (
     <Select.Root>
       <Select.Trigger placeholder="Select a user..." />
       <Select.Content>
         <Select.Group>
-          <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="1">Doyoung Park</Select.Item>
+          <Select.Label>Users</Select.Label>
+          {users.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
