839763edc3038a1e3d50710c089557b25fb61790
Doyoung Park <doyoungp.00@gmail.com>
2024-08-21 15:50:31 +0900

    Add user drop down menu

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,11 +1,18 @@
 "use client";
 
-import { Box, Container, Flex } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
 import classNames from "classnames";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { AiFillBug } from "react-icons/ai";
+import { AiFillBug, AiOutlineUser } from "react-icons/ai";
 
 function NavBar() {
   const { status, data: session } = useSession();
@@ -19,13 +26,13 @@ function NavBar() {
     <nav className="border-b mb-6 px-3 py-3">
       <Container>
         <Flex justify="between">
-          <Flex align="center" gap="4">
-            <Link href="/">
+          <Flex gap="4">
+            <Link href="/" className="content-center">
               <AiFillBug size="1.5rem" />
             </Link>
             <ul className="flex space-x-4">
               {links.map((link) => (
-                <li key={link.href}>
+                <li key={link.href} className="content-center">
                   <Link
                     href={link.href}
                     className={classNames({
@@ -40,25 +47,32 @@ function NavBar() {
               ))}
             </ul>
           </Flex>
-          <Flex justify="end">
-            <Box>
-              {status === "authenticated" && (
-                <Link
-                  href="/api/auth/signout"
-                  className="text-zinc-500 hover:text-zinc-800 transition-colors"
-                >
-                  Log Out
-                </Link>
-              )}
-              {status === "unauthenticated" && (
-                <Link
-                  href="/api/auth/signin"
-                  className="text-zinc-500 hover:text-zinc-800 transition-colors"
-                >
-                  Log In
-                </Link>
-              )}
-            </Box>
+          <Flex content="center">
+            <DropdownMenu.Root>
+              <DropdownMenu.Trigger>
+                <Avatar
+                  src={session?.user?.image!}
+                  fallback={<AiOutlineUser />}
+                  size="2"
+                  radius="full"
+                  className="cursor-pointer"
+                />
+              </DropdownMenu.Trigger>
+              <DropdownMenu.Content>
+                <DropdownMenu.Label>
+                  <Text size="2">{session?.user?.email}</Text>
+                </DropdownMenu.Label>
+                <DropdownMenu.Item>
+                  {status === "authenticated" && (
+                    <Link href="/api/auth/signout">Log Out</Link>
+                  )}
+                  {status === "unauthenticated" && (
+                    <Link href="/api/auth/signin">Log In</Link>
+                  )}
+                </DropdownMenu.Item>
+              </DropdownMenu.Content>
+            </DropdownMenu.Root>
+            <Box></Box>
           </Flex>
         </Flex>
       </Container>
