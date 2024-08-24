eba6eba21f2730123e1dd42f4012cf0b521e9008
Doyoung Park <doyoungp.00@gmail.com>
2024-08-24 16:03:52 +0900

    Update issue detail skeleton

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,22 +1,31 @@
 import { Skeleton } from "@/app/components";
-import { Box, Card, Flex, Heading } from "@radix-ui/themes";
+import { Box, Card, Flex, Grid, Heading } from "@radix-ui/themes";
 
 function IssueDetailsLoadingPage() {
   return (
-    <Box className="max-w-xl">
-      <Heading>
-        <Skeleton />
-      </Heading>
-      <Flex gap="2" my="2">
-        <Skeleton width="4rem" />
-        <Skeleton width="10rem" />
-      </Flex>
-      <Card className="prose" mt="4">
-        <Skeleton width="6rem" />
-        <Skeleton width="16rem" />
-        <Skeleton width="10rem" />
-      </Card>
-    </Box>
+    <Grid columns={{ initial: "1", sm: "5" }} gap="4">
+      <Box className="md:col-span-4">
+        <Heading>
+          <Skeleton />
+        </Heading>
+        <Flex gap="2" my="2">
+          <Skeleton width="4rem" />
+          <Skeleton width="10rem" />
+        </Flex>
+        <Card mt="4">
+          <Skeleton width="6rem" />
+          <Skeleton width="16rem" />
+          <Skeleton width="10rem" />
+        </Card>
+      </Box>
+      <Box>
+        <Flex direction="column" gap="2">
+          <Skeleton />
+          <Skeleton />
+          <Skeleton />
+        </Flex>
+      </Box>
+    </Grid>
   );
 }
 
