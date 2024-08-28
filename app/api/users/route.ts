03a13fbff8faa5440157bbf5ee02d4876a9f7ff5
Doyoung Park <doyoungp.00@gmail.com>
2024-08-28 18:23:36 +0900

    Users api caching

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -3,5 +3,8 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
   const users = await prisma.user.findMany({ orderBy: { name: "asc" } });
-  return NextResponse.json(users);
+
+  const response = NextResponse.json(users);
+  response.headers.set("Cache-Control", "public, max-age=30");
+  return response;
 }
